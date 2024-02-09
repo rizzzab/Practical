375373e042db8a9115ec91d49fa9c48e1589e2d7
rizzab <2022bit057@sgg.ac.in>
2024-02-09 22:50:22 +0530

    Hashcode()

---
+++ hash.java
@@ -0,0 +1,10 @@
+    public class IntegerHashCodeExample1 {  
+        public static void main(String[] args)  
+        {  
+            //Create integer object  
+            Integer i = new Integer("155");  
+            //Returned hash code value for this object   
+            int hashValue = i.hashCode();  
+            System.out.println("Hash code Value for object is: " + hashValue);  
+        }  
+    }  

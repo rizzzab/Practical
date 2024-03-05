79637a92ef5e3f41754aca810eae8c16825549be
rizzab <2022bit057@sgg.ac.in>
2024-03-05 23:52:19 +0530

    Relational operator

---
+++ rel.java
@@ -0,0 +1,27 @@
+import java.io.*;
+ 
+// Main class
+class GFG {
+ 
+    // Main driver method
+    public static void main(String[] args)
+    {
+        // Initializing variables
+        int var1 = 20, var2 = 20, var3 = 10;
+ 
+        // Displaying var1, var2, var3
+        System.out.println("Var1 = " + var1);
+        System.out.println("Var2 = " + var2);
+        System.out.println("Var3 = " + var3);
+ 
+        // Comparing var1 and var2 and
+        // printing corresponding boolean value
+        System.out.println("var1 >= var2: "
+                           + (var1 >= var2));
+ 
+        // Comparing var2 and var3 and
+        // printing corresponding boolean value
+        System.out.println("var2 >= var3: "
+                           + (var2 >= var3));
+    }
+}

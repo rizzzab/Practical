7dfad6eadfb5a9b78465fc067bac5129599931c7
rizzab <2022bit057@sgg.ac.in>
2024-04-04 00:38:51 +0530

    Abstract

---
+++ ab1.java
@@ -0,0 +1,46 @@
+abstract class Subject {
+
+    Subject() { 
+
+      System.out.println("Learning Subject"); 
+
+    }
+
+   
+
+      abstract void syllabus();
+
+   
+
+      void Learn(){
+
+          System.out.println("Preparing Right Now!");
+
+    }
+}
+ 
+
+class IT extends Subject {
+
+  void syllabus(){
+
+    System.out.println("C , Java , C++");
+
+  }
+}
+ 
+
+class GFG {
+
+    public static void main(String[] args) {
+
+        Subject x=new IT();
+
+       
+
+          x.syllabus();
+
+          x.Learn();
+
+    }
+}

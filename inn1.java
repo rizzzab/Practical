bdce65bc1bc25ea74e2b7eaec47f661be6086685
rizzab <2022bit057@sgg.ac.in>
2024-04-12 00:49:24 +0530

    Inner classes

---
+++ inn1.java
@@ -0,0 +1,38 @@
+class Outer {
+ 
+
+    // Method defined inside outer class
+
+    void outerMethod()
+
+    {
+ 
+
+        // Print statement
+
+        System.out.println("inside outerMethod");
+
+    }
+ 
+
+    // Class 2
+
+    // Inner class
+
+    class Inner {
+ 
+
+        // Main driver method
+
+        public static void main(String[] args)
+
+        {
+ 
+
+            // Display message for better readability
+
+            System.out.println("inside inner class Method");
+
+        }
+        }
+}

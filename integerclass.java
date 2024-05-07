8b8a96fda160257ab7cb4e8d21def812f26e9cb5
rizzab <2022bit057@sgg.ac.in>
2024-05-07 20:57:18 +0530

    INTCLASS

---
+++ integerclass.java
@@ -0,0 +1,17 @@
+public final class Integer {
+    private int value;
+
+    public Integer(int value) {
+        this.value = value;
+    }
+
+    public int intValue() {
+        return value;
+    }
+
+    public static Integer valueOf(int value) {
+        return new Integer(value);
+    }
+
+    
+}
